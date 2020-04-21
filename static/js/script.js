a6696f37556f06beea174af380f672ee8b631062
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-04-21 09:16:06 +0530

    adding project assets and generate random card

--- static/js/script.js
+++ static/js/script.js
@@ -151,6 +151,50 @@ function buttonsColorRandom(){
 }
 
 
+//CHALLENGE 5: BLACKJACK GAME
+let blackjackGame={
+    'you': {'scoreSpan':'#your-blackjack-result', 'div':'#your-box', 'score':0},
+    'dealer': {'scoreSpan':'#dealer-blackjack-result', 'div':'#dealer-box', 'score':0},
+    'cards': ['2','3','4','5','6','7','8','9','10','A','J','K','Q'],
+}
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+const CARDS = blackjackGame['cards'];
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click',blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackjackDeal);
+
+function blackjackHit(){
+    let card = randomCard();
+    showCard(card,YOU);
+}
+
+function randomCard(){
+    let randomIndex = Math.floor(Math.random()*13);
+    return CARDS[randomIndex];
+}
+function showCard(card,activePlayer){
+    let cardImage = document.createElement('img');
+    cardImage.src = `static/images/${card}.png`;
+    console.log(cardImage.src);
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+    
+    for(let i=0;i<yourImages.length;i++)
+        yourImages[i].remove();
+
+    for(let i=0;i<dealerImages.length;i++)
+        dealerImages[i].remove();
+}
 
 
 
