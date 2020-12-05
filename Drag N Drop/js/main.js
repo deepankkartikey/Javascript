79b514ce434d044f480df6c5627ce34bfafb5d03
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-05 15:36:33 +0530

    Add drag-over,enter,leave,drop events

--- Drag N Drop/js/main.js
+++ Drag N Drop/js/main.js
@@ -5,6 +5,15 @@ const empties = document.querySelectorAll('.empty')
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Loop through empties and call drag events
+for(const empty of empties)
+{
+    empty.addEventListener('dragover',dragOver)
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave',dragLeave)
+    empty.addEventListener('drop',dragDrop)
+}
+
 function dragStart() {
     console.log('start')
     this.className += ' hold'
@@ -16,3 +25,18 @@ function dragEnd() {
     this.className = 'fill'
 }
 
+function dragOver() {
+    console.log('OVER')
+}
+
+function dragEnter() {
+    console.log('ENTER')
+}
+
+function dragLeave() {
+    console.log('LEAVE')
+}
+
+function dragDrop() {
+    console.log('DROP')
+}
