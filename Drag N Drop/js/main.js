c9daf26a8f26399f985cf169acdcac5bf26268a5
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-05 15:52:16 +0530

    Manipulate drag events and add to container

--- Drag N Drop/js/main.js
+++ Drag N Drop/js/main.js
@@ -15,28 +15,34 @@ for(const empty of empties)
 }
 
 function dragStart() {
-    console.log('start')
+    //console.log('start')
     this.className += ' hold'
     setTimeout(() => this.className = 'invisible', 0)
 }
 
 function dragEnd() {
-    console.log('end')
+    //console.log('end')
     this.className = 'fill'
 }
 
-function dragOver() {
-    console.log('OVER')
+function dragOver(e) {
+    e.preventDefault()
+    //console.log('OVER')
 }
 
-function dragEnter() {
-    console.log('ENTER')
+function dragEnter(e) {
+    //console.log('ENTER')
+    e.preventDefault()
+    this.className+=' hovered'
 }
 
 function dragLeave() {
-    console.log('LEAVE')
+    //console.log('LEAVE')
+    this.className='empty'
 }
 
 function dragDrop() {
-    console.log('DROP')
+    //console.log('DROP')
+    this.className = 'empty'
+    this.append(fill)
 }
