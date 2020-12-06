30295fe9fae7f6b997a97a70345cf86a6e4820a0
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-06 12:43:41 +0530

    Console Time,Log,Table,Warn

---
+++ Learn/Play with Console/js/script.js
@@ -0,0 +1,14 @@
+console.time('Code Took')
+
+console.log('Hello there')
+console.log(30+4)
+console.log(20)
+console.log(true)
+console.log([32,25,12,11])
+
+console.log({name:'Deepank', Age: 24})
+console.table({name:'Deepank', Age: 24})
+
+console.warn('This is a Warning')
+
+console.timeEnd('Code Took')
