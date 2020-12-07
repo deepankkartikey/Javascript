66919ccc0578f671bf0008b6eda74afb5c3d5e83
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-07 20:13:25 +0530

    Variables rules

--- Learn/Variables/js/script.js
+++ Learn/Variables/js/script.js
@@ -2,7 +2,12 @@ console.log('VARIABLES')
 
 // var , let , const
 
+// 1. Can't start with numbers
+// 2. Can start with letters, _ , $
+// 3. Case-sensitive
+
 var name = 'Deepank'
 var channel;
-var marks=34
-console.log(name, channel, marks)
+var marks=34;
+marks=0;
+console.log(name, channel, marks);
