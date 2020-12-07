a8384b27f4d4d08a1365cf723b3e83b10b112321
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-07 20:18:53 +0530

    Const and Let variables

--- Learn/Variables/js/script.js
+++ Learn/Variables/js/script.js
@@ -1,13 +1,25 @@
 console.log('VARIABLES')
 
-// var , let , const
-
 // 1. Can't start with numbers
 // 2. Can start with letters, _ , $
 // 3. Case-sensitive
 
+// var
 var name = 'Deepank'
 var channel;
 var marks=34;
 marks=0;
 console.log(name, channel, marks);
+
+// const
+const owner = 'old'; // initialization is compulsory
+//owner = 'new';  // throws Assignment Error
+console.log(owner)
+
+// let  : block level variable
+let msg = 'outside block'
+console.log(msg)
+{
+    let msg = 'inside block'
+    console.log(msg)
+}
