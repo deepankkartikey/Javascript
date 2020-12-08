96f24f95609cf5c97652ed54035a0c1929660b80
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-08 20:05:05 +0530

    Primitive Datatypes

---
+++ Learn/Datatypes/js/script.js
@@ -0,0 +1,23 @@
+// Primitive: String, numbers, boolean, Null, Undefines, Symbol
+// [MEMORY IN STACK]
+
+// String 
+let firstName = 'Deepank'
+let lastName = "Kartikey"
+console.log(typeof firstName , firstName, lastName)
+
+// Number
+let age = 24
+console.log(typeof age, age)
+
+// Boolean 
+let drive = true
+console.log(typeof drive, drive)
+
+// null
+let nullVar = null
+console.log(typeof nullVar, nullVar)
+
+// undefined
+let undef = undefined
+console.log(typeof undef, undef)
