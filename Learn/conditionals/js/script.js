4f554c5be10f71e235f07d71e4961b026db17258
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-12 00:40:39 +0530

    Conditionals: If-Else

---
+++ Learn/conditionals/js/script.js
@@ -0,0 +1,10 @@
+console.log("CONDITIONALS");
+
+const age = 24;
+if (age == 24) {
+  console.log(`Age is ${age}`);
+} else if (age < 24) {
+  console.log(`Age less than 24`);
+} else {
+  console.log(`Age greater than 24`);
+}
