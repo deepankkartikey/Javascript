f2f35e31947fb2229a8ecbbf9f9670acb7bbd8b1
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-14 20:15:47 +0530

    Function definition

--- Learn/Functions/js/script.js
+++ Learn/Functions/js/script.js
@@ -5,3 +5,12 @@ const from = function (sender = "Anonymous") {
   console.log(`-From ${sender}`);
 };
 
+//function definition
+function greet(name = "Bot") {
+  console.log(`Hey, there. ${name}`);
+  from();
+}
+
+greet("Deepank");
+greet("Arun");
+greet();
