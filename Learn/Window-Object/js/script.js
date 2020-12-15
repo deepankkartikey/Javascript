cd45d27cbbe54a08aca1da3c77c003ee0956c4cc
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-15 14:52:11 +0530

    Window Object

--- Learn/Window-Object/js/script.js
+++ Learn/Window-Object/js/script.js
@@ -1,8 +1,17 @@
 console.log("WINDOW OBJECT");
 
 let a;
-a = alert('Learning...');
-a = prompt('Soon be JS Rookie');
-a = confirm('Done for today?');
+// a = alert('Learning...');
+// a = prompt('Soon be JS Rookie');
+// a = confirm('Done for today?');
+
+a = window.innerHeight;
+a = innerWidth;
+
+a = scrollX;
+a = scrollY;
+
+a = window.history;
+//a = history.go(1);
 
 console.log(a);
