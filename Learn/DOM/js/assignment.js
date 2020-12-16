40c959c5e19eda5fadbe672820afddc4405cd468
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-16 15:02:22 +0530

    Assignment-WebCrawler

---
+++ Learn/DOM/js/assignment.js
@@ -0,0 +1,9 @@
+// Crawl links from a website and display specific links having a searchstring
+
+let resultSet = document.links;
+
+Array.from(resultSet).forEach(function(element){
+    if(!element.href.includes('google')){
+        console.log(element);
+    }
+})
