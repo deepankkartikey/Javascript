7e517b50aab3779c47b99508325edea96fc1fca0
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-17 17:30:01 +0530

    Single Element Selector

--- Learn/DOM/js/domSelect.js
+++ Learn/DOM/js/domSelect.js
@@ -5,3 +5,21 @@ element selectors:
 1. Single Element Selector
 2. Multi Element Selector
 */
+
+let element = document.getElementById('first');
+element.style.backgroundColor = 'yellow';
+element.style.width = '100px';
+element.innerHTML = '<b>Deepank</b>'
+console.log(element);
+console.log(element.id);
+console.log(element.className);
+console.log(element.childNodes);
+console.log(element.parentNode);
+
+
+let sel = document.querySelector('#first');
+sel = document.querySelector('.child');
+sel = document.querySelector('div');  // only first div is selected
+sel.style.color = 'green'; 
+console.log(sel);
+
