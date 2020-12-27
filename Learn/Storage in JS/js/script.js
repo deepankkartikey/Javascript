483cec8a0dbd2e6018069515544bb984fe9e54fd
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-27 23:47:05 +0530

    Local Storage

---
+++ Learn/Storage in JS/js/script.js
@@ -0,0 +1,10 @@
+console.log('STORAGE IN JS');
+
+// Local Storage
+localStorage.setItem('Name','Deepank');
+localStorage.setItem('Name2','Kartikey');
+// localStorage.clear();
+let name = localStorage.getItem('Name');
+console.log(name);
+
+
