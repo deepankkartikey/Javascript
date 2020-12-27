f021c17a64fbd25dbc5e20af3a8b3d8c12ddb29e
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-28 00:04:17 +0530

    Local Storage-Unparse and Parse array to store

--- Learn/Storage in JS/js/script.js
+++ Learn/Storage in JS/js/script.js
@@ -6,5 +6,14 @@ localStorage.setItem('Name2','Kartikey');
 // localStorage.clear();
 let name = localStorage.getItem('Name');
 console.log(name);
+console.log(localStorage);
+localStorage.removeItem('Name2');
+console.log(localStorage);
+
+let veg = ['ginger','onion','tomato'];
+localStorage.setItem('veg',JSON.stringify(veg));
+veg = localStorage.getItem('veg');
+veg = JSON.parse(localStorage.getItem('veg'));
+console.log(veg);
 
 
