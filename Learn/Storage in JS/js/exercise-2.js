fc7177b9fe34143f3c261f97237240f3af4b9c35
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2020-12-30 22:23:52 +0530

    Exercise-2: Editable Div and Local Storage

---
+++ Learn/Storage in JS/js/exercise-2.js
@@ -0,0 +1,24 @@
+console.log("EXERCISE-2");
+
+let newDiv = document.createElement("div");
+let textNode = document.createTextNode("This is editable");
+newDiv.appendChild(textNode);
+newDiv.setAttribute("class", "edit");
+newDiv.setAttribute("name", "editable");
+newDiv.style =
+  "margin: 15px; padding:5px; border: 2px solid black; width: 154px;";
+document.body.appendChild(newDiv);
+
+newDiv.addEventListener("click", function edit() {
+  let noOfTextAreas = document.getElementsByClassName("textarea").length;
+  console.log("CLICKED");
+  if (noOfTextAreas == 0) {
+    newDiv.innerHTML = `<textarea id="textarea" class="textarea">${newDiv.innerHTML}</textarea>`;
+  }
+
+  let textarea = document.getElementById("textarea");
+  textarea.addEventListener("blur", function () {
+    newDiv.innerHTML = textarea.value;
+    localStorage.setItem('TextArea',newDiv.innerHTML);
+  });
+});
