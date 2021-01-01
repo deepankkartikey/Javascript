029d868bdda232e78766040ae9c04916738cc6c4
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2021-01-01 22:19:40 +0530

    Notes App: Building Logic

--- Learn/Project-1 Magic Notes/js/script.js
+++ Learn/Project-1 Magic Notes/js/script.js
@@ -1,2 +1,8 @@
 console.log('Simple Note-Taking App');
 
+// When User adds a note, add to local storage
+let addNote = document.getElementById('addBtn');
+addNote.addEventListener('click', function(e){
+    let noteDesc = document.getElementById('addTxt').value;
+    console.log(noteDesc);
+})
