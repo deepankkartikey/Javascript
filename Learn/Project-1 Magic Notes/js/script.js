11647cbbbbfd6c0c4c5d1295a663919525c43df1
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2021-01-03 15:58:55 +0530

    Show elements form Local storage

--- Learn/Project-1 Magic Notes/js/script.js
+++ Learn/Project-1 Magic Notes/js/script.js
@@ -1,15 +1,17 @@
 console.log("Simple Note-Taking App");
+let notesObj = new Array();
+showNotes();
 
 // When User adds a note, add to local storage
 let addNote = document.getElementById("addBtn");
-let notesObj = new Array();
+
 addNote.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
   if (notes != null) {
     notesObj = JSON.parse(notes);
   }
-  if(addTxt.value){
+  if (addTxt.value) {
     notesObj.push(addTxt.value);
   }
   localStorage.setItem("notes", JSON.stringify(notesObj));
@@ -17,17 +19,21 @@ addNote.addEventListener("click", function (e) {
   showNotes();
 });
 
+
+// show elements from local storage
 function showNotes() {
   let notes = localStorage.getItem("notes");
   console.log(notes);
-  notesObj = JSON.parse(notes);
+  if (notes != null) {
+    notesObj = JSON.parse(notes);
+  }
 
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
         <div class="notecard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Note ${index+1}</h5>
+            <h5 class="card-title">Note ${index + 1}</h5>
             <p class="card-text">${element}</p>
             <button class="btn btn-danger">Delete</button>
         </div>
@@ -36,7 +42,9 @@ function showNotes() {
   });
 
   let notesElem = document.getElementById("notes");
-  if(notes.length!=0){
-      notesElem.innerHTML = html;
+  if (notesObj.length != 0) {
+    notesElem.innerHTML = html;
+  } else {
+    notesElem.innerHTML = `No Notes added!`;
   }
 }
