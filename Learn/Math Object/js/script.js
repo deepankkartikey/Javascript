2ea5499f06635dd6e4695c0a55f8b9bc4e067e25
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2021-01-05 01:39:00 +0530

    Math Obj in JS

---
+++ Learn/Math Object/js/script.js
@@ -0,0 +1,23 @@
+console.log("Math Object in Javascript");
+
+let [x, y] = [3, 6];
+let z;
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.6);
+z = Math.round(5.5);
+z = Math.ceil(5.4);
+z = Math.floor(5.3);
+z = Math.abs(-5.3);
+z = Math.sqrt(5);
+z = Math.pow(2,10);
+z = Math.min(2,13,21,12,11,9,-9);
+// generate random number between 1 to 100
+z = Math.ceil(Math.random()*100);
+z = Math.ceil(50+(100-50)*Math.random());
+console.log(z);
