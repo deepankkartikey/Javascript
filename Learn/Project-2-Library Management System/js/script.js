9ee724b9de3e076a823a06d965980f92cfdceda1
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2021-01-19 01:38:41 +0530

    Library Management-JS

--- Learn/Project-2-Library Management System/js/script.js
+++ Learn/Project-2-Library Management System/js/script.js
@@ -1,1 +1,43 @@
-console.log('Hogward\'s Library');
+console.log("Hogward's Library");
+
+function Book(title, writer, genre) {
+  this.title = title;
+  this.author = writer;
+  this.genre = genre;
+}
+
+let libraryForm = document.getElementById("libraryForm");
+// console.log(libraryForm);
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(event) {
+  event.preventDefault();
+  let bookTitle = document.getElementById("bookName").value;
+  let bookAuthor = document.getElementById("author").value;
+  let bookGenre = getSelectedGenre();
+  // console.log(bookTitle, bookAuthor, bookGenre);
+  let newBook = new Book(bookTitle, bookAuthor, bookGenre);
+  // storeBook(newBook);
+  displayBook(newBook);
+}
+
+function getSelectedGenre() {
+  let genres = document.getElementsByName("genre");
+  for (let genre of genres) {
+    if (genre.checked) {
+      return genre.value;
+    }
+  }
+}
+
+function displayBook(book) {
+  let tableBody = document.getElementById("tableBody");
+  let uiString = `                    
+  <tr>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.genre}</td>
+  </tr>
+  `;
+  tableBody.innerHTML += uiString;
+}
