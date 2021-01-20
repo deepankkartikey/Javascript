bd7653c1d1af38188fe73c71cf28be4f34c0baa7
Deepank Kartikey <deepank.kartikey@aciworldwide.com>
2021-01-20 19:24:05 +0530

    Async Programming

---
+++ Learn/Async Programming/js/script.js
@@ -0,0 +1,8 @@
+console.log('Asynchronous Programming');
+
+for (let index = 0; index < 4500; index++) {
+    const element = index;
+    console.log('This is index: ',index);   
+}
+
+console.log('DONE');
