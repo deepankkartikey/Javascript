a29ba821755085bdfa67d1809cad43f381cb3fcf
Deepank kartikey <deepankkartikey@gmail.com>
2021-01-23 19:58:45 -0800

    Ajax: XMLHttpRequest

--- Learn/Async Programming/js/script.js
+++ Learn/Async Programming/js/script.js
@@ -1,8 +1,27 @@
-console.log('Asynchronous Programming');
+console.log("AJAX");
 
-for (let index = 0; index < 4500; index++) {
-    const element = index;
-    console.log('This is index: ',index);   
+let fetchBtn = document.getElementById("fetchBtn");
+fetchBtn.addEventListener('click', buttonClickHandler);
+
+function buttonClickHandler() {
+  console.log("Fetch Clicked");
+
+  // initialize XHR object
+  let xhr = new XMLHttpRequest();
+
+  // open XHR object
+  xhr.open("GET", "note.txt", true);
+
+  // what to do on progress
+  xhr.onprogress = () => {
+    console.log("On Proogress");
+  };
+
+  // what to do when response is ready
+  xhr.onload = () => {
+    console.log(this.responseText);
+  };
+
+  // send the request
+  xhr.send();
 }
-
-console.log('DONE');
