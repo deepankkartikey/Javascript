0b3fefd1a4a7ee41c74b7b50d790b4fe2f07b056
Deepank kartikey <deepankkartikey@gmail.com>
2021-01-28 20:49:50 +0530

    3D Animation-basic

--- 3D Animation/js/script.js
+++ 3D Animation/js/script.js
@@ -1,1 +1,25 @@
 console.log('3-D');
+
+//Movement Animation to be done
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+//Moving animation effect
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth/2 - e.pageX)/15; 
+    let yAxis = (window.innerHeight/2 - e.pageY)/15;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`; 
+});
+
+
+// Animate In
+container.addEventListener('mouseenter', (e)=> {
+    card.style.transition = 'none';
+})
+
+// Animate Out
+container.addEventListener('mouseleave', (e) => {
+    card.style.transform = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
+
