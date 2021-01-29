c4f48abde5f6040e3a6fc328e201f076c8c85634
Deepank kartikey <deepankkartikey@gmail.com>
2021-01-29 19:40:39 +0530

    Some more effects

--- 3D Animation/js/script.js
+++ 3D Animation/js/script.js
@@ -4,6 +4,12 @@ console.log('3-D');
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 
+const title = document.querySelector('.title');
+const product = document.querySelector('.product img');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
 //Moving animation effect
 container.addEventListener('mousemove', (e) => {
     let xAxis = (window.innerWidth/2 - e.pageX)/15; 
@@ -15,11 +21,23 @@ container.addEventListener('mousemove', (e) => {
 // Animate In
 container.addEventListener('mouseenter', (e)=> {
     card.style.transition = 'none';
+    //PopOut
+    title.style.transform = "translateZ(150px)";
+    product.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)";
 })
 
 // Animate Out
 container.addEventListener('mouseleave', (e) => {
     card.style.transform = 'all 0.5s ease';
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+    title.style.transform = "translateZ(0px)";
+    product.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
 });
 
