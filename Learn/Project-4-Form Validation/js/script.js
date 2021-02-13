a8d2db0097839919bbafa00dfb8d5271a2a81bb7
Deepank kartikey <deepankkartikey@gmail.com>
2021-02-13 14:16:37 +0530

    Form Validation: Submit action

--- Learn/Project-4-Form Validation/js/script.js
+++ Learn/Project-4-Form Validation/js/script.js
@@ -3,6 +3,8 @@ console.log("Welcome to Travel Desk");
 const uname = document.querySelector("#name");
 const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
+const submit = document.querySelector("#submit");
+let [isValidName, isValidEmail, isValidPhone] = [false, false, false];
 
 // console.log(uname,email,phone);
 
@@ -12,7 +14,8 @@ uname.addEventListener("blur", () => {
   let regex = /^[a-zA-Z]([0-9a-zA-Z]){6,10}$/;
   let username = uname.value;
   if (regex.test(username)) {
-    console.log("Username Valid");
+    //console.log("Username Valid");
+    isValidName = true;
     uname.classList.replace("is-invalid", "is-valid");
   } else {
     console.warn("Invalid Username");
@@ -27,6 +30,7 @@ email.addEventListener("blur", () => {
   let emailValue = email.value;
   //console.log(regex.exec(emailValue));
   if (regex.test(emailValue)) {
+    isValidEmail = true;
     email.classList.replace("is-invalid", "is-valid");
   } else {
     console.warn("Email is invalid");
@@ -40,9 +44,25 @@ phone.addEventListener("blur", () => {
   let regex = /^[0-9]{10}$/;
   let phoneNumber = phone.value;
   if (regex.test(phoneNumber)) {
+    isValidPhone = true;
     phone.classList.replace("is-invalid", "is-valid");
   } else {
     console.warn("Invalid Phone Number");
     phone.classList.add("is-invalid");
+  }
+});
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("Submit clicked...");
+  let successAction = document.querySelector("#success");
+  successAction.classList.add("alert", "show");
+  successAction.innerHTML = ``;
+  if (isValidName && isValidEmail && isValidPhone) {
+    successAction.classList.replace("alert-danger", "alert-success");
+    successAction.innerHTML = `<strong>Success!</strong> Your Travel request has been successfully submitted. 😊`;
+  } else {
+    successAction.classList.add("alert-danger");
+    successAction.innerHTML = `<strong>Error!</strong> Username, Email or Phone not valid. 😟`;
   }
 });
